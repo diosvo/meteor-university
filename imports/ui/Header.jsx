24bebc3de12cc0e40890e95e2f198becff85d5dd
Dios Vo <vtmn1212@gmail.com>
2022-06-20 10:28:32 +0700

    feat (header): create basic header

---
+++ imports/ui/Header.jsx
@@ -0,0 +1,32 @@
+import PetsIcon from "@mui/icons-material/Pets";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="medium"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <PetsIcon />
+          </IconButton>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Contacts
+          </Typography>
+          <Button color="inherit">#diosvo</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
