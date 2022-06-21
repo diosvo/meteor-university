9678b8fb51cd3483dcd4815d487696e8fdd9fa78
Dios Vo <vtmn1212@gmail.com>
2022-06-21 14:31:22 +0700

    feat: create wallets and transactions collections

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import "../imports/api/contacts/index";
+import "../imports/api/transactions/index";
+import "../imports/api/wallets/index";
 
 Meteor.startup(() => {});
