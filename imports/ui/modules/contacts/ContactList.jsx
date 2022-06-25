29cebca4e4eead3ee635ea5f2482250e80a2220b
Dios Vo <vtmn1212@gmail.com>
2022-06-25 23:28:22 +0700

    style (part-3)
    - update UI for list contacts

--- imports/ui/modules/contacts/ContactList.jsx
+++ imports/ui/modules/contacts/ContactList.jsx
@@ -1,7 +1,17 @@
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import { Meteor } from "meteor/meteor";
 import { useFind, useSubscribe } from "meteor/react-meteor-data";
+
+import ArchiveIcon from "@mui/icons-material/Archive";
+import DeleteIcon from "@mui/icons-material/DeleteOutline";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
 import ContactsCollection from "../../../api/contacts/ContactsCollection";
 import LoadingProgress from "../../libs/LoadingProgress";
 
@@ -35,40 +45,57 @@ export default ContactList = () => {
 
   const ContactItem = React.memo(({ contact }) => {
     return (
-      <li>
-        {contact.name} - {contact.email} - {contact.walletId}
-        <Button
-          size="small"
-          variant="text"
-          color="inherit"
-          onClick={(event) => archive(event, contact._id)}
-        >
-          Archive
-        </Button>
-        <Button
-          size="small"
-          variant="text"
-          color="inherit"
-          onClick={(event) => remove(event, contact._id)}
-        >
-          Remove
-        </Button>
-      </li>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt={contact.name} src={contact.imageUrl} />
+          </ListItemAvatar>
+          <ListItemText primary={contact.name} secondary={contact.email} />
+
+          {/* Actions */}
+
+          <Tooltip title="Archive">
+            <IconButton
+              size="medium"
+              aria-label="archive"
+              onClick={(event) => archive(event, contact._id)}
+            >
+              <ArchiveIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Remove">
+            <IconButton
+              color="error"
+              size="medium"
+              aria-label="delete"
+              onClick={(event) => remove(event, contact._id)}
+            >
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
     );
   });
 
   return (
-    <div>
-      <h2>Contact List</h2>
-      <Stack spacing={2} direction="row">
-        <Button variant="text" onClick={deleteAll}>
-          Delete All
-        </Button>
-      </Stack>
+    <>
+      <Typography variant="overline" display="block" gutterBottom>
+        Contact List
+      </Typography>
 
-      {contacts.map((contact) => (
+      <Divider component="div" />
+
+      {contacts.length < 1 && (
+        <Typography my={2} fontSize={14} variant="body2">
+          No contacts found.
+        </Typography>
+      )}
+
+      {contacts.map((contact, index) => (
         <ContactItem key={contact._id} contact={contact} />
       ))}
-    </div>
+    </>
   );
 };
